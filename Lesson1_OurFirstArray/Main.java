a193e711a001158c0f0beed87c7376b32a1de94d
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-20 10:24:23 -0500

    Completed 1.1 cities

--- Lesson1_OurFirstArray/Main.java
+++ Lesson1_OurFirstArray/Main.java
@@ -2,10 +2,10 @@ package Lesson1_OurFirstArray;
 
 
 /**
- * Write a description of class Main here.
+ * Creates data of three cities' populations and the least amount of daily sun they can get.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/20/26
  */
 public class Main
 {
@@ -13,7 +13,18 @@ public class Main
     public static void main(String[] args)
     {
         // Create the 3 arrays here
-        
+        String[] cities = {"Las Vegas", "Minsk", "Sao Paulo"};
+        int[] populations = {667501, 2038822, 12330000};
+        double[] sunshine = {8.11, 1.36, 6.05};
         // Print all 3 arrays according to the output in the description
+        for (int i = 0 ; i < cities.length; i++)
+        {
+            System.out.println(cities[i] + "'s population is " + populations[i] + ".");
+        }
+        
+        for (int i = 0 ; i < cities.length; i++)
+        {
+            System.out.println("The least amount of sunshine " + cities[i] + " gets is " + sunshine[i] + ".");
+        }
     }
 }
