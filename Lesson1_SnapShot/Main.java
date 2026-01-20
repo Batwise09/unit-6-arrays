8fae5d7f1b27f70c2cbc3b5588c3be8a347b4e27
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-20 10:38:31 -0500

    Completed 1.4 splash screen

--- Lesson1_SnapShot/Main.java
+++ Lesson1_SnapShot/Main.java
@@ -2,15 +2,36 @@ package Lesson1_SnapShot;
 
 
 /**
- * Write a description of class Main here.
+ * Displays the splash screen for Snap Shot
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/20/26
  */
 public class Main
 {
     public static void main(String[] args)
     {
+        String[] lines = 
+        {
+            "Welcome",
+            "to",
+            "the snap shot",
+            "app!",
+        };
         
+        for (int i = 0; i < lines.length; i++)
+        {
+            System.out.println(lines[i]);
+        }
+        
+        System.out.println();
+        
+        lines[0] = "Upgrade";
+        lines[lines.length - 1] = "premium app!";
+        
+        for (int i = 0; i < lines.length; i++)
+        {
+            System.out.println(lines[i]);
+        }
     }
 }
