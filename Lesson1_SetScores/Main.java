2e904bb644508a43d1d67c58c3e495ab4a0aa8cd
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-20 10:28:53 -0500

    Completed 1.2 scores

--- Lesson1_SetScores/Main.java
+++ Lesson1_SetScores/Main.java
@@ -4,13 +4,28 @@ package Lesson1_SetScores;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/20/26
  */
 public class Main
 {
     public static void main(String[] args)
     {
+        int[] scores = {80, 95, 82, 67, 100};
         
+        for (int i = 0; i < scores.length; i++)
+        {
+            System.out.println(scores[i]);
+        }
+        
+        System.out.println(); //Skip line
+        
+        scores[2] = 72;
+        scores[scores.length - 1] = 95;
+        
+        for (int i = 0; i < scores.length; i++)
+        {
+            System.out.println(scores[i]);
+        }
     }
 }
