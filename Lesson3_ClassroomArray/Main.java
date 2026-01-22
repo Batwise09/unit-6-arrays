c1c5015d0d2d6d5d091a214bed4c581174eb4759
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-22 10:47:32 -0500

    Completed 3.3 students

--- Lesson3_ClassroomArray/Main.java
+++ Lesson3_ClassroomArray/Main.java
@@ -2,18 +2,26 @@ package Lesson3_ClassroomArray;
 
 
 /**
- * Write a description of class Main here.
+ * Creates 5 students, puts them in an array and prints them all out.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/22/26
  */
 public class Main
 {
-        public static void main(String[] args)
+    public static void main(String[] args)
     {
-
-
+        Student bbj = new Student("Billy Bob", "Joe", 11);
+        Student ddbj3j = new Student ("Demetrius Demarcus Bartholomew", "James III Jr.", 5);
+        Student spiderman = new Student("Peter", "Parker", 12);
+        Student mj = new Student("Michael", "Jackson", 12);
+        Student finalBoss = new Student("M̷̧̨̧̜̗̠̝͉͚̺̝̗̗͉̣͇̼̪̫̔̈i̵̡̨̫͉̲͔̘͕͓̤͕͐́͋́̀̈́̃̒̍̈̏͛́͂̕͘͝͠t̶̜̆͆h̸̛̯̗͈̬̙̥̖̼͇͓̦̘͍̭̔̾́͊́͊͆̊͊͋̀͘̚͝͝r̵̟̤̤̥̖͓̔͑̇̇̊̎̐̅͆̒̈̓́̍̿͊̽̚͝͝į̶͎̜̱̖̩͚͖̣͖͈͓̭̖͐̈̇͗̃͐̉͆͗̿͜͜͝x̶̛̦͗̓̑̅̓́͛̉͒̔͗͂̚͝͝͠", "Ķ̷̧̧̧̰̝̻̮̬̤͖̠̱͔̀́̀̓̍̋̑̊̅́̅̾i̸̧̢̨̡̥̖̣̼̣̜̬̯̯̫̻̫̗̙̹͖͙̜̯͆͋͒͐̌̈́͝ņ̷͍͈̠̳͗̃g̵̢̡̧̨̡̨̧̛̺̤̞̜̫̯̖͓̜̠̹͓̪͓̾͂͗̑͝ ̶̨͈̠̲̏͋̎̐̈͐͌̋̑͗͠͠ǫ̵̠͎̮̙̹͔͖̜͈͍̹̰̗̙̙̥͍͕̝̺̈́̊̔͒͛̅͆̆̋̿͗͒̀͗̚̕͜͝ͅf̴̨̮͎́̅̂̈́͊͋̌̈́̿̈́̈͛̾͛̀̿͊͗̓͠͠͝ ̴̨̛͔͕͖͕̦̙̞̹̻̮̳̯͚̫̰͎̘͂̔́̍̈́̇̀̔̿̓̈́̿̈̊̿͗͊̀͗͋͝ͅN̶̛̻̗̹̺̽̈́͒o̴̧͈̩̟̩̻̙̮̟̙͗͂̌͛̐͆̈́͑͒̐̈͐̈̓̌̐̌̈́̾̚͝ẗ̴͚͎͓̣̦̪̥̯͎͕͖͙͔̺́̃́̅̒̀̓̂̑̿̓͒̕̚̕̚̚͝ͅh̸̨̦͕͙͎̮͕̫͓̯̩̺̘̲͚̻͎͖́̽́̈͌͊́̋ï̶̧̛̪̜̤̩̀͛̐͝n̵̡̯͚̤͎̱̹̥͙̬̩̜͉͇̘̩̞͕̓͗͌͗̔͑̎̐̚͝g̴̢̞͊̊̓̈́̃̍̔̾͌̌͋͌̾͛̽͊͘̕̚͜", 67);
         
+        Student[] students = {bbj, ddbj3j, spiderman, mj, finalBoss};
         
+        for (Student pupil : students)
+        {
+            System.out.println(pupil);
+        }
     }
 }
--- Lesson3_ClassroomArray/Student.java
+++ Lesson3_ClassroomArray/Student.java
@@ -2,12 +2,41 @@ package Lesson3_ClassroomArray;
 
 
 /**
- * Write a description of class Student here.
+ * A Student has a name and grade level
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/22/26
  */
 public class Student
 {
-
+    String firstName;
+    String lastName;
+    int gradeLevel;
+    
+    public Student(String firstName, String lastName, int gradeLevel)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeLevel = gradeLevel;
+    }
+    
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    
+    public String getLastName()
+    {
+        return lastName;
+    }
+    
+    public int getgradeLevel()
+    {
+        return gradeLevel;
+    }
+    
+    public String toString()
+    {
+        return "Grade " + gradeLevel + ": " + firstName + " " + lastName;
+    }
 }
