51d8ec6bdf66dd49c0af507a9bbaf896b37e0e3c
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-22 10:10:40 -0500

    Completed 3.1 odd numbers

--- Lesson3_PrintOdds/Main.java
+++ Lesson3_PrintOdds/Main.java
@@ -4,8 +4,8 @@ package Lesson3_PrintOdds;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/22/26
  */
 public class Main
 {
@@ -14,11 +14,11 @@ public class Main
     {
         int[ ] values = {17, 34, 56, 2, 19, 100};
         
-        for (int i = 0; i < values.length; i++)
+        for (int num : values)
         {
-            if (values[i] % 2 == 1)
+            if (num % 2 == 1)
             {
-                System.out.println(values[i] + " is odd");
+                System.out.println(num + " is odd");
             }
         }
     }
