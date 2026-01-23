196b71a69b3a372fb28bd2be72fd8f13b94b81cf
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-23 10:25:23 -0500

    Started 3.3 exam progress

--- Lesson4_StudentProgressTracker/Student.java
+++ Lesson4_StudentProgressTracker/Student.java
@@ -4,14 +4,46 @@ package Lesson4_StudentProgressTracker;
 /**
  * Write a description of class Student here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/23/26
  */
 public class Student
 {
+    String firstName;
+    String lastName;
+    int gradeLevel;
+    double gpa;
+    int[] exams;
+    int numExamsTaken = 0;
+    final int NUM_EXAMS = 4;
     
     
-
+    public Student(String firstName, String lastName, int gradeLevel)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeLevel = gradeLevel;
+        
+        exams = new int[NUM_EXAMS];
+    }
     
+    public int getExamImprovement()
+    {
+        int improvement = 0;
+        for (int i = 1; i < exams.length; i++)
+        {
+            improvement += exams[i] - exams[i - 1];
+        }
+        return improvement;
+    }
     
+    public String getName()
+    {
+        return firstName + " " + lastName;
+    }
+    
+    public void addExamScore(int score)
+    {
+        
+    }
 }
