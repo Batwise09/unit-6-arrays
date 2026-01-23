361c6df90a5863a75f7dc38ce5957a3b456d2a16
Batwise09 <160027218+Batwise09@users.noreply.github.com>
2026-01-23 08:59:28 -0500

    Completed 4.1 median

--- Lesson4_FindTheMedian/Main.java
+++ Lesson4_FindTheMedian/Main.java
@@ -2,10 +2,10 @@ package Lesson4_FindTheMedian;
 import java.util.*;
 
 /**
- * Write a description of class Main here.
+ * Prints the median values of arrays.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author - Samwise Shurtleff
+ * @version - 1/23/26
  */
 public class Main
 {
@@ -18,10 +18,75 @@ public class Main
         System.out.print("\nThe median value of the ODD array is " + median(numbers2));
         
     }
-
+    
     public static double median(int[] arr)
     {
-        // your code goes here!
-        return -1;
+        int[] newArr = getSortedArray(arr);
+        int halfIdx = arr.length / 2;
+        if (arr.length % 2 != 0)
+        {
+            return (double) newArr[halfIdx];
+        }
+        else
+        {
+            return ((double) newArr[halfIdx - 1] + (double) newArr[halfIdx]) / 2.0;
+        }
+    }
+    
+    public static int[] getSortedArray(int[] arr)
+    {
+        //Make deep copy of old array
+        int[] oldArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++)
+        {
+            oldArr[i] = arr[i];
+        }
+        
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < newArr.length; i++)
+        {
+            int lowest = oldArr[0];
+            for (int num : oldArr)
+            {
+                if (num < lowest)
+                {
+                    lowest = num;
+                }
+            }
+            for (int j = 0; j < oldArr.length; j++)
+            {
+                if (oldArr[j] == lowest)
+                {
+                    oldArr[j] = Integer.MAX_VALUE;
+                }
+            }
+            newArr[i] = lowest;
+        }
+        return newArr;
+    }
+    
+    
+    
+    
+    
+    //Debug method to see arrays
+    public static void debugPrintArray(int[] arr, String name)
+    {
+        System.out.print(name + " ");
+        for (int num : arr)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void debugPrintArray(int[] arr)
+    {
+        System.out.print("Array: ");
+        for (int num : arr)
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
